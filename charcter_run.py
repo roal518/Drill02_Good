98e4e9c467e56ef1e216c83eb2bb0b6c04d17810
roal518 <roalqwerty@gmail.com>
2023-09-20 17:43:32 +0900

    무한루프 구조 완성

--- charcter_run.py
+++ charcter_run.py
@@ -3,4 +3,18 @@ import math
 open_canvas()
 grass=load_image('grass.png')
 character=load_image('character.png')
+#문제의조건을 먼저 제대로 인식하자.
+#pass는 아무 일도 안하는 c로 따지면 {}와 같다.
+
+def run_circle():
+    print("circle")
+    pass
+def run_rectangle():
+    printf("rectangle")
+    pass
+
+while True:
+    run_circle()
+    run_rectangle()
+    break
 close_canvas()
