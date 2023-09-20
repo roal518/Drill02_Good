cc41b43a72809d3efa2981d82438c68cec5a1b7a
roal518 <roalqwerty@gmail.com>
2023-09-20 17:57:49 +0900

    drawing circle done

--- charcter_run.py
+++ charcter_run.py
@@ -12,6 +12,18 @@ def run_circle():
     grass.draw_now(400,30)
     character.draw_now(400,90)
     delay(1)
+#의도를 포함해주면 좋다.
+    cx = 800/2
+    cy = 600/2
+    r = 200
+#for 문의 기능 알아두자.
+    for deg in range(0,360,5):
+        x=cx+r*math.cos(math.radians(deg))
+        y=cy+r*math.sin(math.radians(deg))
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,y)
+        delay(0.01)
     pass
 def run_rectangle():
     print("rectangle")
